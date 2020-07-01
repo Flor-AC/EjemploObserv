e87712978a18fca13fded9a1cc153ef9cfef3eab
flor <flor@flor-HP-15-Notebook-PC>
2020-07-01 13:04:16 -0500

    Nueva clase

---
+++ src/ejemploobserv/HiloCliente.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploobserv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.awt.windows.ThemeReader;
+
+/**
+ *
+ * @author flor
+ */
+public class HiloCliente extends Observable implements Runnable{
+    
+    private boolean isStoped = false;
+    private Socket cliente;
+    private BufferedReader receptor = null;
+    private PrintWriter emisor=null;
+    
+    public HiloCliente(Socket cliente){
+        
+        try {
+            this.cliente = cliente;
+            InputStreamReader isr;
+            isr = new InputStreamReader(cliente.getInputStream());
+            receptor = new BufferedReader(isr);
+            emisor = new PrintWriter(cliente.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public synchronized void stoped(){
+        isStoped = true;
+    }
+    
+    @Override
+    public void run() {
+        while(isStoped){
+            try {
+                setChanged();
+                
+                emisor.write("hola mundo\n");
+                emisor.flush();
+                notifyObservers("Valor");
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
---
+++ src/ejemploobserv/ServerThread.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploobserv;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author flor
+ */
+public class ServerThread extends Thread{
+
+    private ServerSocket server = null;
+    private boolean isStoped =false;
+    private Observer observer;
+    
+    public ServerThread(Observer observer) throws IOException{ //Desencadenar
+        server = new ServerSocket(7700);
+        this.observer = observer;
+    }
+    
+    public synchronized void stoped(){
+        isStoped = true;
+    }
+    
+    @Override
+    public void run() {
+        Socket cliente = null;
+        while(isStoped){
+            try {
+                cliente = server.accept();
+                HiloCliente hc = new HiloCliente(cliente);
+                hc.addObserver(observer);
+                Thread hilo = new Thread(hc);
+                hilo.start();
+            } catch (IOException ex) {
+                stoped();
+                Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        super.run(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
